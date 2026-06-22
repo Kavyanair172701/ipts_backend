c2277ccbb7ad99eb504066d04fc9c487a15f4b02
KAVYA <kavyanairrudran27@gmail.com>
2026-06-22 12:14:09 +0530

    new changes

--- ion_note.py
+++ ion_note.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from models import IonNote
 
 router = APIRouter(prefix="/ion-note", tags=["ION NOTE"])
+
 
 @router.post("/create")
 def create_ion_note(
@@ -35,16 +36,68 @@ def create_ion_note(
         base_amount=base_amount,
         gst_percent=gst_percent,
         gst_amount=gst_amount,
-        grand_total=grand_total,
+        grand_total=grand_total
     )
 
     db.add(new_ion)
     db.commit()
     db.refresh(new_ion)
 
-    return {"message": "ION Note Saved Successfully", "data": new_ion}
+    return {
+        "message": "ION Note Saved Successfully",
+        "data": new_ion
+    }
 
 
 @router.get("/")
 def get_all_ion_notes(db: Session = Depends(get_db)):
-    return db.query(IonNote).order_by(IonNote.ion_note_id.desc()).all()
+    return (
+        db.query(IonNote)
+        .order_by(IonNote.ion_note_id.desc())
+        .all()
+    )
+
+
+@router.get("/{ion_note_id}")
+def get_ion_note(
+    ion_note_id: int,
+    db: Session = Depends(get_db)
+):
+    ion_note = (
+        db.query(IonNote)
+        .filter(IonNote.ion_note_id == ion_note_id)
+        .first()
+    )
+
+    if not ion_note:
+        raise HTTPException(
+            status_code=404,
+            detail="ION Note Not Found"
+        )
+
+    return ion_note
+
+
+@router.delete("/{ion_note_id}")
+def delete_ion_note(
+    ion_note_id: int,
+    db: Session = Depends(get_db)
+):
+    ion_note = (
+        db.query(IonNote)
+        .filter(IonNote.ion_note_id == ion_note_id)
+        .first()
+    )
+
+    if not ion_note:
+        raise HTTPException(
+            status_code=404,
+            detail="ION Note Not Found"
+        )
+
+    db.delete(ion_note)
+    db.commit()
+
+    return {
+        "message": "ION Note Deleted Successfully"
+    }
